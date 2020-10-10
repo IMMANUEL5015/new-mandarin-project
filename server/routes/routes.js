e1b7a943fdf3254cac253f69b99b683d33d46006
IMMANUEL5015 <immanueldiai@gmail.com>
2020-10-10 12:45:28 +0100

    Handle Non-Existent Routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,15 @@ const authRoutes = require('../routes/authentication');
 const productsRoutes = require('../routes/products');
 const foodOrdersRoutes = require('../routes/foodOrders');
 const globalErrorHandler = require('../middlewares/errors');
+const AppError = require('../utilities/appError');
 
 router.use('/api/v1', authRoutes);
 router.use('/api/v1/products', productsRoutes);
 router.use('/api/v1/food_orders', foodOrdersRoutes);
+
+router.all('*', (req, res, next) => {
+    return next(new AppError('The page you requested cannot be found!', 404));
+});
 
 router.use(globalErrorHandler);
 
